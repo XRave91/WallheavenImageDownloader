a20c1aa098f2d0b8c1e7923492eb2a9be147b40e
nightrain912 <nightrain912@gmail.com>
2017-02-15 11:45:50 +0300

    Update Saver.java
    всякое

--- src/Xrave/Saver.java
+++ src/Xrave/Saver.java
@@ -6,5 +6,7 @@ import java.io.BufferedInputStream;
  * Created by 2 on 16.01.2017.
  */
 interface Saver {
-    boolean saveFile(BufferedInputStream data, String filename);
+    // потому что нам не важно, куда мы сохраняем. это вполне может быть дропбокс или вк
+    // тогда мы заранее зададим все настройки и будем сохранять по имени
+    boolean saveData(BufferedInputStream data, String name);
 }
