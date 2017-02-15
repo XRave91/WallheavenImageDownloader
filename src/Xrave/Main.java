c9c3e1cde5a26fede10c77ccdd4c51ba54124e6d
nightrain912 <nightrain912@gmail.com>
2017-02-15 11:35:20 +0300

    Чисто и аккуратно, не?

--- src/Xrave/Main.java
+++ src/Xrave/Main.java
@@ -1,28 +1,41 @@
-package Xrave;
+// глянь сюда https://docs.oracle.com/javase/tutorial/java/package/namingpkgs.html
+package com.xrave.wall_downloader;
 
 import java.lang.reflect.MalformedParametersException;
 
 public class Main {
-    private static class InputParameters {
-        int countOfPictures;
-        String[] tags;
-        InputParameters(String[] arguments) throws MalformedParametersException{
-            if (arguments.length<1){
-                throw new MalformedParametersException("syntax of this downloader  \"dowl.java %countOfPictures% %tags with space delimiters%\"");
-            }
-            countOfPictures=Integer.parseInt(arguments[0]);
-            if (countOfPictures<1){
-                throw new MalformedParametersException("incorrect count Of Pictures");
-            }
-            tags=new String[arguments.length-1];
-            for (int i =1;i<arguments.length;i++){
-                tags[i-1]=arguments[i];
-            }
-        }
-    }
-    public static void main(String[] args) {
-	WallHeavenAggregator aggregator=new WallHeavenAggregator(new HttpLoader(),new FileSaver());
-	InputParameters parameters =new InputParameters(args);
-	aggregator.getImages(parameters.countOfPictures,parameters.tags);
-    }
+	private class InputParameters {
+		private int countOfPictures;
+		private String[] tags;
+
+		InputParameters(String[] arguments) throws MalformedParametersException {
+			if (arguments.length < 1)
+				throw new MalformedParametersException("syntax of this downloader  \"dowl.java %countOfPictures% %tags with space delimiters%\"");
+
+			countOfPictures = Integer.parseInt(arguments[0]);
+			if (countOfPictures < 1)
+				throw new MalformedParametersException("incorrect count Of Pictures");
+
+			tags = new String[arguments.length - 1];
+			for (int i = 1; i < arguments.length; ++i)
+				tags[i - 1] = arguments[i];
+		}
+		
+		public int getCountOfPictures() {
+			return countOfPictures;
+		}
+
+		public String[] getCountOfPictures() {
+			// тут все равно можно изменить состояние (содержимое массива)
+			// можно сделать как тут: http://stackoverflow.com/questions/2419353/make-arraylist-read-only
+			return tags;
+		}
+	}
+
+	public static void main(String[] args) {
+		InputParameters parameters = new InputParameters(args);
+
+		WallHeavenAggregator aggregator = new WallHeavenAggregator(new HttpLoader(), new FileSaver());
+		aggregator.getImages(parameters.countOfPictures, parameters.tags);
+	}
 }
