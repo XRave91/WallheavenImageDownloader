495e6755027e90d427905c6d30728a0de2481e62
nightrain912 <nightrain912@gmail.com>
2017-02-15 12:39:57 +0300

    Update HttpLoader.java
    чел, я уже задолбался

--- src/Xrave/HttpLoader.java
+++ src/Xrave/HttpLoader.java
@@ -1,4 +1,4 @@
-package Xrave;
+package com.xrave.wall_downloader;
 
 import java.io.*;
 import java.net.URL;
@@ -9,61 +9,29 @@ import java.nio.file.Files;
  * Created by 2 on 07.01.2017.
  */
 public class HttpLoader implements Loader {
-    @Override
-    public BufferedInputStream getData(String url) {
-        return getContent(url);
-    }
+	// кстати, а она будет публичной или приватной? проверь
+	static final String userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11";
 
-    static String userAgent="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11";
-    BufferedInputStream getContent(String url){
-        URLConnection connection = null;
-        try {
-            URL link=new URL(url);
-            connection = link.openConnection();
-            connection.setRequestProperty("User-Agent",userAgent);//записать в константу
-            connection.connect();
-        } catch (Exception e) {
-            System.out.println("Соединение не удалось. Проверьте соединение, и правильность УРЛ адреса");
-            return null;
-        }
-        BufferedInputStream bis = null;
-        try {
-            bis = new BufferedInputStream(connection.getInputStream());
-        } catch (IOException e) {
-            return null;
-        }
-        if (bis ==null){
-            return null;
-        }
-        return bis;
-        }
+	@Override
+	public BufferedInputStream getData(String path) {
+		URLConnection connection = null;
+		try {
+			URL link = new URL(path);
+			connection = link.openConnection();
+			connection.setRequestProperty("User-Agent", userAgent);//записать в константу
+			connection.connect();
+		} catch (Exception e) {
+			// почему на русском?
+			System.out.println("Соединение не удалось. Проверьте соединение, и правильность УРЛ адреса");
+			return null;
+		}
 
-//    void download_images(String[] urls,String Folder){//логичнее это перенести в класс парсинга
-//        for (int i=0;i<urls.length;i++) {
-//            System.out.println("\n"+(i+1)+"/"+fileId.size()+" "+String.join("_",parameters.tags)+"_"+fileId.get(i));
-//            try {
-//                if(!downloadFile(new URL("https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-"+fileId.get(i)+
-//                        ".jpg"),new File("c:\\wallpapes\\"+String.join("_",parameters.tags)+"_"+fileId.get(i)+".jpg"))){
-//                    try {
-//                        Files.deleteIfExists(new File("c:\\wallpapes\\"+String.join("_",parameters.tags)+"_"+
-//                                fileId.get(i)+".jpg").toPath());
-//                    } catch (IOException e3) {
-//                        System.out.println("file not deleted");
-//                        e3.printStackTrace();
-//                    }
-//                    try {
-//                        downloadFile(new URL("https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-"+
-//                                fileId.get(i)+".png"),new File("c:\\wallpapes\\"+String.join("_",parameters.tags)+"_"+fileId.get(i)+".png"));
-//                    } catch (MalformedURLException e) {
-//                        e.printStackTrace();
-//                    }
-//                    System.out.println(i+1+"/"+fileId.size()+" "+String.join("_",parameters.tags)+"_"+fileId.get(i)+".png downloaded");
-//                    continue;
-//                }
-//            } catch (MalformedURLException e) {
-//                e.printStackTrace();
-//            }
-//            System.out.println(i+1+"/"+fileId.size()+" "+String.join("_",parameters.tags)+"_"+fileId.get(i)+".jpg downloaded");
-//        }
-//    }
+		// тут было великолепно:
+		// если null, вернем null, если нет - вернём значение
+		try {
+			return new BufferedInputStream(connection.getInputStream());
+		} catch (IOException e) {
+			return null;
+		}
+	}
 }
